1426cfac6d65aa69f3abd5691eaf556949c08a9d
kimizuka <f.kimizuka@gmail.com>
2023-09-12 11:30:48 +0900

    add next-pwa

---
+++ src/next-pwa/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  assetPrefix: '/next-pwa/',
+  distDir: 'next-pwa',
+  trailingSlash: true
+};
+
+module.exports = nextConfig;
